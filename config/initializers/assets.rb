3b076da61246160b15951926060514f6ccaea0b1
Tectual <arash@tectual.com.au>
2014-10-15 22:54:06 +1100

    fix assets compile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,2 @@
-Rails.application.config.assets.precompile += %w( application.css bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( application.css )
 Rails.application.config.assets.precompile += %w( application.js  )
